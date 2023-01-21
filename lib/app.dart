0e41a8b8c002003b242ea57010e81080247b8135
charleston <charles1235223@gmail.com>
2023-01-21 09:59:35 -0400

    add home_page

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:consumindo_api_jsonplaceholder/modules/home/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
